4e2cd32ac1b8415910989aced5b3d6101b228bdf
Surya_K_Ratheesh <7suryakr@gmail.com>
2026-08-07 00:49:16 +0530

    Arrays

--- 08_No_of_Islands(200).py
+++ 08_No_of_Islands(200).py
@@ -1,6 +1,13 @@
 from collections import deque
 
 def main():
+    # grid = [
+    #     ["1","1","1","1","0"],
+    #     ["1","1","0","1","0"],
+    #     ["1","1","0","0","0"],
+    #     ["0","0","0","0","0"]
+    # ]
+
     grid = [
         ["1","1","0","0","0"],
         ["1","1","0","0","0"],
@@ -14,22 +21,22 @@ def no_of_islands(grid):
     if not grid:
         return 0
 
-    def bfs(r,c):
+    def bfs(r, c):
         q = deque()
         visit.add((r,c))
         q.append((r,c))
 
         while q:
             row, col = q.popleft()
-            directions = [[1,0],[-1,0],[0,1],[0,-1]]
+            directions = [[1,0], [-1,0], [0,1], [0,-1]]
 
             for dr, dc in directions:
-                r, c = row+dr, col+dc
+                r, c = dr + row, dc + col
 
                 if (r in range(rows) and c in range(cols) and grid[r][c] == "1" and (r,c) not in visit):
                     q.append((r,c))
                     visit.add((r,c))
-    
+
     count = 0
     rows = len(grid)
     cols = len(grid[0])
@@ -37,7 +44,6 @@ def no_of_islands(grid):
 
     for r in range(rows):
         for c in range(cols):
-
             if grid[r][c] == "1" and (r,c) not in visit:
                 bfs(r,c)
                 count += 1
